b31995050ca74421dd95e0c9f2d26f5b09fb616b
Unknown <prpr48@naver.com>
2021-06-06 23:57:05 +0900

    add sample gql query

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import { gql } from '@apollo/client';
 import styles from '../styles/Home.module.css';
 
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
 export default function Home() {
-  return <div className={styles.container}>Hello World</div>;
+  return (
+    <div className={styles.container}>
+      <h2>My first Apollo app 🚀</h2>
+    </div>
+  );
 }
